5835a09d0e7b85407371605af2f728e90e9fec01
Alvaro Gamez Chavez <gaositd@gmail.com>
2024-04-14 16:33:46 -0600

    add bootstrap to project with yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 Rails.start()
 Turbolinks.start()
